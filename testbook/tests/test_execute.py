e74a27dc5068a7465ac7a2ceee579c25c1f504f3
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-06-13 22:29:06 +0530

    removed faulty prerun logic

--- testbook/tests/test_execute.py
+++ testbook/tests/test_execute.py
@@ -4,30 +4,37 @@ from testbook import notebook_loader
 def test_execute_cell():
     with notebook_loader('testbook/tests/resources/foo.ipynb') as notebook:
         notebook.execute_cell(1)
-        assert notebook.cell_output_text(1) == 'hello world\n[1, 2, 3]\n'
+        assert notebook.cell_output_text(1) == 'hello world\n[1, 2, 3]'
 
         notebook.execute_cell([2, 3])
-        assert notebook.cell_output_text(3) == 'foo\n'
+        assert notebook.cell_output_text(3) == 'foo'
 
 
 def test_execute_cell_tags():
     with notebook_loader('testbook/tests/resources/foo.ipynb') as notebook:
         notebook.execute_cell('test1')
-        assert notebook.cell_output_text('test1') == 'hello world\n[1, 2, 3]\n'
+        assert notebook.cell_output_text('test1') == 'hello world\n[1, 2, 3]'
 
         notebook.execute_cell(['prepare_foo', 'execute_foo'])
-        assert notebook.cell_output_text('execute_foo') == 'foo\n'
+        assert notebook.cell_output_text('execute_foo') == 'foo'
 
 
 @notebook_loader("testbook/tests/resources/foo.ipynb")
-def test_notebook(notebook):
+def test_notebook_loader(notebook):
     notebook.execute_cell('test1')
-    assert notebook.cell_output_text('test1') == 'hello world\n[1, 2, 3]\n'
+    assert notebook.cell_output_text('test1') == 'hello world\n[1, 2, 3]'
 
     notebook.execute_cell(['prepare_foo', 'execute_foo'])
-    assert notebook.cell_output_text('execute_foo') == 'foo\n'
+    assert notebook.cell_output_text('execute_foo') == 'foo'
 
 
-@notebook_loader("testbook/tests/resources/foo.ipynb", prerun='test1')
-def test_notebook_with_prerun(notebook):
-    assert notebook.cell_output_text(1) == 'hello world\n[1, 2, 3]\n'
+@notebook_loader("testbook/tests/resources/foo.ipynb", prerun='prepare_foo')
+def test_notebook_loader_with_prerun(notebook):
+    notebook.execute_cell('execute_foo')
+    assert notebook.cell_output_text('execute_foo') == 'foo'
+
+
+def test_notebook_loader_with_prerun_context_manager():
+    with notebook_loader("testbook/tests/resources/foo.ipynb", prerun='prepare_foo') as notebook:
+        notebook.execute_cell('execute_foo')
+        assert notebook.cell_output_text('execute_foo') == 'foo'
