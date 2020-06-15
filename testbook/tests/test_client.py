f969d59df8ddbddcfde8dc6c2f49379afa406edd
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-06-15 09:27:46 +0530

    added unit tests

--- testbook/tests/test_client.py
+++ testbook/tests/test_client.py
@@ -18,3 +18,29 @@ def test_cell_index_raises_error(cell_index_args, expected_error):
     with testbook('testbook/tests/resources/inject.ipynb') as notebook:
         with pytest.raises(expected_error):
             notebook._cell_index(cell_index_args)
+
+
+@pytest.mark.parametrize(
+    "cell_tag, var_name, expected_result",
+    [
+        ('dict', 'sample_dict', {'foo': 'bar'}),
+        ('list', 'sample_list', ['foo', 'bar']),
+        ('list', 'sample_list + ["hello world"]', ['foo', 'bar', 'hello world']),
+        ('int', 'sample_int', 42),
+        ('int', 'sample_int * 2', 84),
+        ('str', 'sample_str', 'hello world'),
+        ('str', 'sample_str + " foo"', 'hello world foo'),
+    ],
+)
+def test_value(cell_tag, var_name, expected_result):
+    with testbook('testbook/tests/resources/inject.ipynb', prerun=cell_tag) as notebook:
+        assert notebook.value(var_name) == expected_result
+
+
+@pytest.mark.parametrize(
+    "cell_tag, code", [('int', 'sample_int *= 2'), ('int', 'print(sample_int)'),],
+)
+def test_value_raises_error(cell_tag, code):
+    with testbook('testbook/tests/resources/inject.ipynb', prerun=cell_tag) as notebook:
+        with pytest.raises(ValueError):
+            notebook.value(code)
