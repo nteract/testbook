4d29930595527f69444392ebf57a6d5ce0d5fd62
alm <alonme@users.noreply.github.com>
2021-05-19 17:28:41 +0300

    Update branch to main

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ html_theme = 'sphinx_book_theme'
 html_theme_options = {
     "path_to_docs": "docs",
     "repository_url": "https://github.com/nteract/testbook",
-    "repository_branch": "master",
+    "repository_branch": "main",
     "use_edit_page_button": True,
 }
 
