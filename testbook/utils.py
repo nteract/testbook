08b57af33df599a60b56f9dbf30e8151bbb4fd22
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2021-05-23 19:34:45 -0700

    DOC add docstring under random_varname, all_subclasses functions (#129)
    Contributes to #105

--- testbook/utils.py
+++ testbook/utils.py
@@ -3,10 +3,32 @@ import string
 
 
 def random_varname(length=10):
+    """
+    Creates a random variable name as string of a given length.
+    This is used in testbook to generate temporary variables within the notebook.
+
+    Parameters
+    ----------
+        length (int)
+
+    Returns:
+    --------
+        random variable name as string of given length
+    """
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
 
 
 def all_subclasses(klass):
+    """
+    This is a function that returns a generator.
+
+    Inspects subclasses associated with a given class in a recursive manner
+    and yields them, such that subclasses of subclasses will be yielded.
+
+    Parameters:
+    -----------
+        klass
+    """
     for subklass in klass.__subclasses__():
         yield subklass
         yield from all_subclasses(subklass)
